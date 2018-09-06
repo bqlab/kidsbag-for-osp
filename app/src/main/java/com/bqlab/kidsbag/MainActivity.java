bef3e21169fd4f79ebca6d1d886e83d44ff4f965
BQ Lab <beaqon@naver.com>
2018-09-06 20:37:52 +0900

    Initial commit

---
+++ app/src/main/java/com/bqlab/kidsbag/MainActivity.java
@@ -0,0 +1,47 @@
+package com.bqlab.kidsbag;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+    private final int ACCESS_FINE_LOCATION = 0;
+    private final int ACCESS_COARSE_LOCATION = 1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        requestPermission(Manifest.permission.ACCESS_FINE_LOCATION, this.ACCESS_FINE_LOCATION);
+        requestPermission(Manifest.permission.ACCESS_COARSE_LOCATION, this.ACCESS_COARSE_LOCATION);
+    }
+
+    public void requestPermission(String permission, int requestCode) {
+        if (ContextCompat.checkSelfPermission(this, permission) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{permission}, requestCode);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case ACCESS_FINE_LOCATION:
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED)
+                    Log.d("PERMISSION", "Allowed permission ACCESS_FINE_LOCATION");
+                else
+                    Log.d("PERMISSION", "DENIED permission ACCESS_FINE_LOCATION");
+
+            case ACCESS_COARSE_LOCATION:
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED)
+                    Log.d("PERMISSION", "Allowed permission ACCESS_FINE_LOCATION");
+                else
+                    Log.d("PERMISSION", "DENIED permission ACCESS_FINE_LOCATION");
+
+        }
+    }
+}
