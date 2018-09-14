989150f757cdfe76652860f9692fa6bbb54a5282
BQ Lab <beaqon@naver.com>
2018-09-14 17:38:53 +0900

    메인함수에 있는것들 메소드로뺌

--- app/src/main/java/com/bqlab/kidsbag/MainActivity.java
+++ app/src/main/java/com/bqlab/kidsbag/MainActivity.java
@@ -8,20 +8,40 @@ import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     private final int ACCESS_FINE_LOCATION = 0;
     private final int ACCESS_COARSE_LOCATION = 1;
 
+    TextView mainStatus;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        requestPermission(Manifest.permission.ACCESS_FINE_LOCATION, this.ACCESS_FINE_LOCATION);
-        requestPermission(Manifest.permission.ACCESS_COARSE_LOCATION, this.ACCESS_COARSE_LOCATION);
+        requestPermissions();
+        setMembers();
     }
 
-    public void requestPermission(String permission, int requestCode) {
+    public void setMainStatus() {
+
+    }
+
+    public void setMembers() {
+        mainStatus = findViewById(R.id.main_status);
+    }
+
+    public void requestPermissions() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, this.ACCESS_FINE_LOCATION);
+        }
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, this.ACCESS_COARSE_LOCATION);
+        }
+    }
+
+    public void callRequestPermission(String permission, int requestCode) {
         if (ContextCompat.checkSelfPermission(this, permission) != PackageManager.PERMISSION_GRANTED) {
             ActivityCompat.requestPermissions(this, new String[]{permission}, requestCode);
         }
@@ -35,7 +55,6 @@ public class MainActivity extends AppCompatActivity {
                     Log.d("PERMISSION", "Allowed permission ACCESS_FINE_LOCATION");
                 else
                     Log.d("PERMISSION", "DENIED permission ACCESS_FINE_LOCATION");
-
             case ACCESS_COARSE_LOCATION:
                 if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED)
                     Log.d("PERMISSION", "Allowed permission ACCESS_FINE_LOCATION");
