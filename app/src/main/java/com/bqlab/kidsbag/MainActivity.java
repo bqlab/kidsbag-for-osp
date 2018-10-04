b738c589b0f5e5f36bcf33d19503d9aeb3c699f8
BQ Lab <beaqon@naver.com>
2018-10-04 21:16:07 +0900

    노티피케이션 만듦, 화면에는 안 나옴

--- app/src/main/java/com/bqlab/kidsbag/MainActivity.java
+++ app/src/main/java/com/bqlab/kidsbag/MainActivity.java
@@ -1,16 +1,23 @@
 package com.bqlab.kidsbag;
 
-import android.Manifest;
+import android.annotation.SuppressLint;
+import android.annotation.TargetApi;
 import android.app.AlertDialog;
 import android.app.FragmentManager;
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.app.Service;
+import android.content.Context;
 import android.content.DialogInterface;
-import android.content.pm.PackageManager;
+import android.content.Intent;
+import android.graphics.Color;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
+import android.os.Build;
 import android.support.annotation.NonNull;
-import android.support.v4.app.ActivityCompat;
-import android.support.v4.content.ContextCompat;
+import android.support.v4.app.NotificationCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -51,6 +58,9 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
     TextView mainTemperature;
     GoogleMap googleMap;
 
+    NotificationManager notificationManager;
+    NotificationChannel notificationChannel;
+
     FirebaseDatabase firebaseDatabase = FirebaseDatabase.getInstance();
     DatabaseReference databaseReference = firebaseDatabase.getReference();
 
@@ -59,6 +69,7 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         setMembers();
+        checkAndroidVersion();
     }
 
     @Override
@@ -111,8 +122,10 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
                             @Override
                             public void onClick(DialogInterface dialog, int which) {
                                 switch (e.getText().toString()) {
-                                    case "hey":
-                                        databaseReference.child("test").push().setValue("hey");
+                                    case "ini":
+                                        databaseReference.child("temp").setValue(0);
+                                        databaseReference.child("v").setValue(0);
+                                        databaseReference.child("v1").setValue(0);
                                         break;
                                     case "cel-def":
                                         databaseReference.child("temp").setValue(28);
@@ -120,11 +133,8 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
                                     case "cel-oh":
                                         databaseReference.child("temp").setValue(40);
                                         break;
-                                    case "bz-true":
+                                    case "bz":
                                         databaseReference.child("buzz").setValue(true);
-                                        break;
-                                    case "bz-false":
-                                        databaseReference.child("buzz").setValue(false);
                                         break;
                                     case "map-se":
                                         databaseReference.child("v").setValue(37.5);
@@ -151,6 +161,8 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         FragmentManager fragmentManager = getFragmentManager();
         MapFragment mapFragment = (MapFragment) fragmentManager.findFragmentById(R.id.main_map);
         mapFragment.getMapAsync(this);
+
+        notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
 
         databaseReference.addValueEventListener(new ValueEventListener() {
             @Override
@@ -198,14 +210,7 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         if (buzz && !isBuzzed) {
             isBuzzed = true;
             databaseReference.child("buzz").setValue(false);
-            new AlertDialog.Builder(MainActivity.this)
-                    .setMessage(R.string.main_buzz)
-                    .setPositiveButton("확인", new DialogInterface.OnClickListener() {
-                        @Override
-                        public void onClick(DialogInterface dialog, int which) {
-                            dialog.dismiss();
-                        }
-                    }).show();
+            makeNotification("디바이스의 부저 버튼을 눌렀습니다.");
         }
         if (!buzz && isBuzzed)
             isBuzzed = false;
@@ -221,5 +226,40 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         }
         Toast.makeText(this, "인터넷이 연결되어 있지 않습니다.", Toast.LENGTH_LONG).show();
         finishAffinity();
+    }
+
+    public void checkAndroidVersion() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            notificationChannel = new NotificationChannel("em", "긴급알림", NotificationManager.IMPORTANCE_DEFAULT);
+            notificationChannel.setDescription("긴급한 상황을 알립니다.");
+            notificationChannel.enableLights(true);
+            notificationChannel.enableVibration(true);
+            notificationChannel.setLightColor(Color.GREEN);
+            notificationChannel.setVibrationPattern(new long[]{100, 200, 100, 200});
+            notificationChannel.setLockscreenVisibility(Notification.VISIBILITY_PRIVATE);
+            notificationManager.createNotificationChannel(notificationChannel);
+        }
+    }
+
+    public void makeNotification(String content) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            notificationManager.notify(0, new NotificationCompat.Builder(this, "em")
+                    .setSmallIcon(R.mipmap.ic_launcher)
+                    .setContentTitle(content)
+                    .setWhen(System.currentTimeMillis())
+                    .setDefaults(Notification.DEFAULT_ALL)
+                    .setAutoCancel(true)
+                    .setPriority(NotificationCompat.PRIORITY_HIGH)
+                    .build());
+        } else {
+            notificationManager.notify(0, new Notification.Builder(this)
+                    .setSmallIcon(R.mipmap.ic_launcher)
+                    .setContentTitle(content)
+                    .setWhen(System.currentTimeMillis())
+                    .setDefaults(Notification.DEFAULT_ALL)
+                    .setAutoCancel(true)
+                    .setPriority(Notification.PRIORITY_HIGH)
+                    .build());
+        }
     }
 }
